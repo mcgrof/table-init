4630692803d130b4d85f9e2d9ab154fcd5b82d0f
Luis R. Rodriguez <mcgrof@suse.com>
2015-11-27 10:57:34 -0800

    kernel: add BIT() and WARN_ON()
    These will be used later.
    
    Signed-off-by: Luis R. Rodriguez <mcgrof@suse.com>

--- kernel.h
+++ kernel.h
@@ -1,7 +1,17 @@
 #include <stdio.h>
 
+#define BIT(nr)		(1UL << (nr))
+
 #define BUG() do {										\
 	fprintf(stderr, "----------------------------------------------------------\n");	\
 	fprintf (stderr, "BUG on %s at %s: %i\n", __func__, __FILE__, __LINE__);		\
+}												\
+while (0)
+
+#define WARN_ON(__test) do {									\
+	if (__test) {										\
+		fprintf(stderr, "----------------------------------------------------------\n");\
+		fprintf (stderr, "WARN_ON on %s at %s: %i\n", __func__, __FILE__, __LINE__);	\
+	}											\
 }												\
 while (0)
